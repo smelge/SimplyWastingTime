298b72b7b22227bda398d87776ec046bdbdbf0b1
smelge <traildogfilms@gmail.com>
2015-12-07 23:44:29 +0000

    Update news_database.php

--- includes/news_database.php
+++ includes/news_database.php
@@ -1,7 +1,7 @@
 <?php
-	$hostname = "10.169.0.14";
-	$username = "simplywa_swtmain";
-	$password = "p{^mTp$~Jfpa";
+	$hostname = "";
+	$username = "";
+	$password = "";
 
 	$dbnews = mysqli_connect($hostname, $username, $password);
 	mysqli_select_db($dbnews, "simplywa_news") or die ("Could not connect to News Server");
