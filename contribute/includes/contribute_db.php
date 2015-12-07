dc9787ac2bf907645e8d3a02b7030613a18f89fb
smelge <traildogfilms@gmail.com>
2015-12-07 23:45:00 +0000

    Update contribute_db.php

--- contribute/includes/contribute_db.php
+++ contribute/includes/contribute_db.php
@@ -1,7 +1,7 @@
 <?php
-	$hostname = "10.169.0.14";
-	$username = "simplywa_contrib";
-	$password = "R_W2PEZ.930oKp";
+	$hostname = "";
+	$username = "";
+	$password = "";
 
 	$dbcont = mysqli_connect($hostname, $username, $password);
 	mysqli_select_db($dbcont, "simplywa_news") or die ("Could not connect to News Server");
