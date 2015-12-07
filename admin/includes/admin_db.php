92d4cba6c9ff60e890164c204aaf05a0c1123ef7
smelge <traildogfilms@gmail.com>
2015-12-07 23:45:38 +0000

    Update admin_db.php

--- admin/includes/admin_db.php
+++ admin/includes/admin_db.php
@@ -1,7 +1,7 @@
 <?php
-	$hostname = "10.169.0.14";
-	$username = "simplywa_admin";
-	$password = "*%kgkgCF85Hy8%%*JG9";
+	$hostname = "";
+	$username = "";
+	$password = "";
 
 	$dbadmin = mysqli_connect($hostname, $username, $password);
 	mysqli_select_db($dbadmin, "simplywa_news") or die ("Could not connect to Admin Server");
