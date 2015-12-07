ef13ced795fd8b2477aa3405c9fac4e517154c10
smelge <traildogfilms@gmail.com>
2015-12-07 23:44:05 +0000

    Update member_database.php

--- includes/member_database.php
+++ includes/member_database.php
@@ -1,7 +1,7 @@
 <?php
-	$hostname = "10.169.0.14";
-	$username = "simplywa_ly2h";
-	$password = "K(JZdF0nMm44*~8";
+	$hostname = "";
+	$username = "";
+	$password = "";
 
 	$dbmembers = mysqli_connect($hostname, $username, $password);
 	mysqli_select_db($dbmembers, "simplywa_ly2h") or die ("Could not connect to Member Server");
